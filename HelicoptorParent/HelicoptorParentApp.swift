e671f8bfd2216ff5c8cdd5ecd2e57679c06215fd
Jake Simpson <jacobsimpsondev@gmail.com>
2025-03-12 20:39:26 -0400

    Initial Commit

---
+++ HelicoptorParent/HelicoptorParentApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelicoptorParentApp.swift
+//  HelicoptorParent
+//
+//  Created by Jacob on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct HelicoptorParentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
